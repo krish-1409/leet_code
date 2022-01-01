27708733680dc3e122e5b24549a0d2f7d62ed375
Sai Krishna Lanka <saikrishnalanka75@gmail.com>
2022-01-01 14:57:50 +0530

    Time: 33 ms (19.10%), Space: 14.1 MB (73.29%) - LeetHub

---
+++ n-th-tribonacci-number/n-th-tribonacci-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def tribonacci(self, n: int) -> int:
+        if n==0:
+            return 0
+        if n==1 or n==2:
+            return 1
+        trib = [0]*(n+1)
+        trib[1] = 1
+        trib[2] = 1
+        for i in range(3,n+1):
+            trib[i] = trib[i-1] + trib[i-2] + trib[i-3]
+        return trib[n]
