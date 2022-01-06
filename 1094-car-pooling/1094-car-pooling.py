5462acb2dbd1a4d631ab99c99e534e59cbcb96d0
Sai Krishna Lanka <saikrishnalanka75@gmail.com>
2022-01-06 15:12:27 +0530

    Time: 68 ms (69.03%), Space: 14.9 MB (18.85%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.py
+++ 1094-car-pooling/1094-car-pooling.py
@@ -1,3 +1,11 @@
+"""
+Maintain an array which contains all the stops.
+Basically this array contains the net num passengers at ith position.
+suppose there is a trip with [3,1,3] declared 0 index array becomes [0,3,0,-3]
+
+At last start calculating sum of the array by iterating upto 1001 at any point if it exceeds capacity then return false.
+
+"""
 class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         cap = [0] * (1001)
