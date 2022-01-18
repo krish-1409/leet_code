6574d78a3861680080d03702b1cd4176e6897254
Sai Krishna Lanka <saikrishnalanka75@gmail.com>
2022-01-18 19:43:43 +0530

    Time: 50 ms (27.58%), Space: 14.4 MB (56.52%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.py
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -3,19 +3,18 @@ class Solution:
         n = len(strs)
         ans= ""
         i = 0
-        while i<200:
+        while i<200:   #gievn maximum characters in each string is 200 so iterating upto 200
             j = 0
-            while j<n:
-                
+            while j<n:  #for each ith character iterating over all the n strings
                 try:
-                    if strs[j][i] == strs[0][i]:
+                    if strs[j][i] == strs[0][i]:  #if equal move onto next string and compare
                         j+=1
                         continue
-                    else:
+                    else:                           #if not equal curretn ans will be returned
                         return ans
-                except:
+                except:                          #if any of the string ends at any point ans can be returned as                                                    #prefix can be of legth atmost the length of min size string
                     return ans
                 j+=1
-            ans+=strs[0][i]
+            ans+=strs[0][i]                     #if iterates over all strings and comes out then we can add ith charactyer to ans
             i+=1
         return ans
