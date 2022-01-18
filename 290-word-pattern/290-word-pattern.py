ff95874bd60457efefa53f346bb9e57a7fafa1e5
Sai Krishna Lanka <saikrishnalanka75@gmail.com>
2022-01-18 20:27:06 +0530

    Time: 57 ms (6.69%), Space: 14.3 MB (23.81%) - LeetHub

--- 290-word-pattern/290-word-pattern.py
+++ 290-word-pattern/290-word-pattern.py
@@ -7,7 +7,7 @@ class Solution:
             return False
         i = 0
         dic = {}
-        vis = set()
+        vis = set()   #if there is a case with two equal words for different letters in pattern, In vis we store each words given in the string if in pattern the letter is matched to word in a string then we add to vis if the same word is matched to different letter in pattern then we can return false.
         while i<n:
             if pattern[i] not in dic:
                 if s[i] in vis:
