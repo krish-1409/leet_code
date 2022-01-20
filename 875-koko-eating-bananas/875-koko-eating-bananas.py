aa0ee9605923edfac872b91f5dcb1df988fac7e9
Sai Krishna Lanka <saikrishnalanka75@gmail.com>
2022-01-20 13:12:16 +0530

    Time: 685 ms (28.90%), Space: 15.5 MB (87.05%) - LeetHub

--- 875-koko-eating-bananas/875-koko-eating-bananas.py
+++ 875-koko-eating-bananas/875-koko-eating-bananas.py
@@ -6,6 +6,16 @@ def getTime(piles,t):
     return ans
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        """
+        The min time to eat up all bananas willbe equal to no. of piles as each pile atleast 
+        tskes an hour to complete. So at his min time the rate will be maximum which inturn \
+        is equal to max height of the pile.
+        we need to minimise rate acc to question
+        Therefore we need to take rates from 1 to max_rate
+        
+        we take each rate and check we can optimize this using binary search.
+        
+        """
         max_rate = max(piles)
         min_rate = 1
         
