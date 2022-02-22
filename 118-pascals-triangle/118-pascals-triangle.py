588de0bb3e9ec0a0073e3e78dcaf09bccacff294
Sai Krishna Lanka <saikrishnalanka75@gmail.com>
2022-02-22 23:43:33 +0530

    Time: 61 ms (10.20%), Space: 13.9 MB (88.58%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -0,0 +1,15 @@
+class Solution:
+    def generate(self, n: int) -> List[List[int]]:
+        ans = [[1],[1,1]]
+        if n==1:
+            return [[1]]
+        if n==2:
+            return ans
+        for _ in range(2,n):
+            curr = [1]
+            prev = ans[-1]
+            for i in range(1,len(prev)):
+                curr.append(prev[i]+prev[i-1])
+            curr.append(1)
+            ans.append(curr)
+        return ans
